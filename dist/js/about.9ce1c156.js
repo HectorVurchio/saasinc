00ed6000db747fc6a31ae63e1904aa793200edf1
Hector Vurchio <hectorvurchio@gmail.com>
2022-10-27 12:13:19 -0400

    dist folder

---
+++ dist/js/about.9ce1c156.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksasinc"]=self["webpackChunksasinc"]||[]).push([[443],{7381:function(n,s,a){a.r(s),a.d(s,{default:function(){return l}});var u=a(3396);const t={class:"about"},c=(0,u._)("h1",null,"This is an about page",-1),e=[c];function r(n,s){return(0,u.wg)(),(0,u.iD)("div",t,e)}var i=a(89);const o={},f=(0,i.Z)(o,[["render",r]]);var l=f}}]);
+//# sourceMappingURL=about.9ce1c156.js.map
