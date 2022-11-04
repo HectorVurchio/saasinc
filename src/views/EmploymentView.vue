e3b0819485301c66086fb1e36e2e7f6f80ffdbb0
Hector Vurchio <hectorvurchio@gmail.com>
2022-11-04 12:38:08 -0400

    Ectrance and employment

--- src/views/EmploymentView.vue
+++ src/views/EmploymentView.vue
@@ -1,6 +1,111 @@
 <template>
-  <div class="services">
-    <p>Apply here</p>
+  <div class="container-fluid employment">
+    <div class="row">
+      <div class="col">
+        <h1>WE'RE HIRING!</h1>
+        <h3>Let's go and jump into...</h3>
+        <p>
+          Here we have some different positions on which you can apply and join
+          us to be part of our professional staff...
+        </p>
+      </div>
+    </div>
+    <!-- First Row -->
+    <div class="row">
+      <div class="card">
+        <div class="card-header fs-4 bg-dark text-white">Carpenter</div>
+        <div class="card-body">
+          <h5 class="card-title">Qualifications:</h5>
+          <div class="card-text">
+            <ul>
+              <li>High school diploma or GED (required)</li>
+              <li>
+                A minimum of 1-year experience as a carpenter in the
+                constructionindustry
+              </li>
+              <li>The ability to communicate effectively</li>
+              <li>
+                Willingness to work in high pressure situations and projects
+                with tight deadlines
+              </li>
+              <li>
+                Ability to work with various materials on the worksite in a
+                knowledgeable fashion
+              </li>
+              <li>Valid Driver's License</li>
+              <li>Ability to pass a federal background check</li>
+            </ul>
+          </div>
+          <p>Are you the talent we are looking for?...</p>
+          <a href="#" class="btn btn-dark">Apply</a>
+        </div>
+      </div>
+    </div>
+    <!-- Second Row -->
+    <div class="row">
+      <div class="card">
+        <div class="card-header fs-4 bg-dark text-white">Electrician</div>
+        <div class="card-body">
+          <h5 class="card-title">Qualifications:</h5>
+          <div class="card-text">
+            <ul>
+              <li>4 years of professional experience as an electrician</li>
+              <li>
+                10 years of supervision experience in commercial or light
+                industrial installations, including healthcare work
+              </li>
+              <li>Proficient in reading blue prints, working independently</li>
+              <li>
+                Conducting all installations and maintenance within current NEC
+                requirements
+              </li>
+              <li>
+                Have your own set of personnel hand tools, and have dependable
+                transportation
+              </li>
+              <li>
+                Experienced in scheduling and working with technology including
+                but not limited to electronic tablets and laptops utilizing
+                Microsoft OUTLOOK, WORD, EXCEL among other software while also
+                having an OSHA 10 minimum certification
+              </li>
+            </ul>
+          </div>
+          <p>Are you the talent we are looking for?...</p>
+          <a href="#" class="btn btn-dark">Apply</a>
+        </div>
+      </div>
+    </div>
+    <!-- Third Row -->
+    <div class="row">
+      <div class="card">
+        <div class="card-header fs-4 bg-dark text-white">Plumbing</div>
+        <div class="card-body">
+          <h5 class="card-title">Qualifications:</h5>
+          <div class="card-text">
+            <ul>
+              <li>Proven experience as a plumber</li>
+              <li>
+                Solid understanding of water distribution and disposal systems
+                in residential, commercial and industrial buildings
+              </li>
+              <li>
+                Ability to handle plumbing tools and equipment (pipe wrenches,
+                pipe cutters, sewer machines etc.)
+              </li>
+              <li>Good communication and interpersonal skills</li>
+              <li>Friendly with a great deal of patience</li>
+              <li>
+                Good physical condition and strength with the ability to work in
+                tight and/or awkward spaces
+              </li>
+            </ul>
+          </div>
+          <p>Are you the talent we are looking for?...</p>
+          <a href="#" class="btn btn-dark">Apply</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,3 +114,30 @@ export default {
   components: {},
 };
 </script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Mulish:ital,wght@1,300&display=swap");
+.employment {
+  width: 70%;
+  margin-bottom: 150px;
+}
+.employment > div:first-child h1,
+.employment > div:first-child h3 {
+  font-family: "Roboto", sans-serif;
+}
+.employment > div:first-child p {
+  font-family: "Mulish", sans-serif;
+  font-size: 26px;
+  line-height: 1.1;
+}
+.card {
+  margin-top: 20px;
+}
+.card .card-header,
+.card a {
+  font-family: "Roboto", sans-serif;
+}
+.card-text {
+  text-align: left;
+}
+</style>
--- src/views/EntranceView.vue
+++ src/views/EntranceView.vue
@@ -1,6 +1,35 @@
 <template>
-  <div class="entrance">
-    <p>Employees Only</p>
+  <div class="container-fluid entrance">
+    <div class="row align-items-center">
+      <div class="col-sm-6 welcome">
+        <img src="@/assets/entrance.png" alt="" />
+      </div>
+      <div class="col-sm-6">
+        <form action="#">
+          <h2>WELCOME!</h2>
+          <div class="form-floating my-4">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+          <button type="submit" class="btn btn-dark my-4 w-50">Submit</button>
+          <p><a href="#">Forgot your password?</a></p>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,3 +38,34 @@ export default {
   components: {},
 };
 </script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap");
+.entrance {
+  width: 80%;
+  margin-bottom: 100px;
+}
+.welcome {
+  background: black;
+  padding-top: 80px;
+  padding-bottom: 80px;
+}
+img {
+  width: 100%;
+}
+form {
+  width: 70%;
+  margin: auto;
+}
+form h2 {
+  font-family: "Roboto", sans-serif;
+  text-align: left;
+  margin-bottom: 50px;
+}
+form a {
+  text-decoration: none;
+  color: black;
+}
+form p {
+  text-align: right;
+}
+</style>
